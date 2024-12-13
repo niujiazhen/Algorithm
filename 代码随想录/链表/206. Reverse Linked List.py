a47ec797d08dfe79fcf26bac746b1afe31094631
niujiazhen <niujiazhen26@163.com>
2024-12-13 17:56:02 +0800

    代码随想录： 206

---
+++ 代码随想录/链表/206. Reverse Linked List.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+from typing import Optional
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+
+
+if __name__ == '__main__':
+    solution=Solution()
+
