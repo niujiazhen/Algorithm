3bdcea02a3559c827dd331e5a43379c8d8fad4f9
niujiazhen <niujiazhen26@163.com>
2025-09-11 20:00:21 -0700

    9/11 26

---
+++ LeetCode Top Interview 150/Array&String/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=j=0
+        ans=[]
+        while j<len(nums):
+            j=i+1
+            if nums[i]==nums[j]
+                j+=1
+
+
+
+
+
+if __name__ == '__main__':
+    solution=Solution()
+    print(solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+
+
