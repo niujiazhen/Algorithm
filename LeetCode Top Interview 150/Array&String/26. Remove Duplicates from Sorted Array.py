e737d6a6ca0ad246a1d2cee1f9ba91e17400121d
niuji <niujiazhen26@163.com>
2025-09-12 00:56:25 -0700

    9/11 26

--- LeetCode Top Interview 150/Array&String/26. Remove Duplicates from Sorted Array.py
+++ LeetCode Top Interview 150/Array&String/26. Remove Duplicates from Sorted Array.py
@@ -3,12 +3,27 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i=j=0
-        ans=[]
-        while j<len(nums):
-            j=i+1
-            if nums[i]==nums[j]
-                j+=1
+        # Time Complexity=O(N), Space Complexity=O(N)
+        # i=j=0 #two pointers
+        # ans=[]
+        # while j<len(nums):
+        #     j+=1
+        #     while j<len(nums) and nums[j]==nums[i]:
+        #         j+=1
+        #     ans.append(nums[i])
+        #     i=j
+        # nums[:len(ans)] = ans[:]
+        # return len(ans)
+
+        # Time Complexity=O(N), Space Complexity=O(1)
+        inserIndex=1 #we just add the first different element to inserIndex
+        i=1
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[i-1]:# it means nums[i] is the first different element and can be added to insertIndex
+                nums[inserIndex]=nums[i]
+                inserIndex+=1
+        return inserIndex
+
 
 
 
@@ -16,6 +31,8 @@ class Solution:
 
 if __name__ == '__main__':
     solution=Solution()
-    print(solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+    nums=[0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    print(solution.removeDuplicates(nums))
+    print(nums)
 
 
