4d1006c21fece31a1bc38c5be768d34425b26113
niujiazhen <niujiazhen26@163.com>
2025-09-16 21:35:37 -0700

    9/16

--- LeetCode Top Interview 150/Array&String/125. Valid Palindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-
-
-
-if __name__ == '__main__':
-    solution=Solution()
-    print(solution.isPalindrome("A man, a plan, a canal: Panama"))
