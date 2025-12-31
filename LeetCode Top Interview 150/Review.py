ba04f63ae9e7ce53842fa6b4c19f3c72924e0ade
niuji <niujiazhen26@163.com>
2025-12-31 05:22:22 -0800

    2025/12/28 Review Two Pointers / HashMap New: 30, 76

--- LeetCode Top Interview 150/Review.py
+++ LeetCode Top Interview 150/Review.py
@@ -2,38 +2,25 @@ from typing import List
 
 
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #Two pointers T=O(n2) S=O(1)
-        nums.sort()
-        ans=[]
-        n=len(nums)
+    def longestConsecutive(self, nums: List[int]) -> int:
+        maxLen=0
+        hash=set(nums)
+        for num in hash:
+            if num-1 not in hash:# means current num is the start of a sequence
+                curNum=num
+                curLen=1
+                while curNum+1 in hash:
+                    curNum+=1
+                    curLen+=1
+                maxLen=max(maxLen,curLen)
 
-        # We first fix one number
-        for i in range(n-2):
-            if nums[i]>0:
-                break
-            if i>0 and nums[i]==nums[i-1]:# remove the duplicated elements of first number
-                continue
-            l=i+1
-            r=n-1
-            while l<r:
-                sum=nums[i]+nums[l]+nums[r]
-                if sum>0:
-                    r-=1
-                elif sum<0:
-                    l+=1
-                else:
-                    ans.append([nums[i],nums[l],nums[r]])
-                    while l<r and nums[l]==nums[l+1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r-1]:
-                        r-=1
-                    l+=1
-                    r-=1
+        return maxLen
 
-        return ans
+
+
+
 
 
 if __name__ == '__main__':
     solution=Solution()
-    print(solution.threeSum([-1,0,1,2,-1,-4]))
+    print(solution.longestConsecutive([100,4,200,1,3,2]))
