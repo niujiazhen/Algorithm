7456f4775f63c8d485c19674cea68774943409a5
niuji <niujiazhen26@163.com>
2026-01-09 03:02:36 -0800

    2026/1/8 Review Graph General

--- LeetCode Top Interview 150/Review.py
+++ LeetCode Top Interview 150/Review.py
@@ -1,27 +1,47 @@
-# Definition for a binary tree node.
-from inspect import stack
-from time import sleep
-from typing import Optional, List
+from collections import defaultdict
+from typing import List
 
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        #Method2 Iterative Traverse: we only have to traverse the first k th element
-        stack=[]
-        while True:
-            while root:
-                stack.append(root)
-                root=root.left# keep pushing smaller node into stack
-            root=stack.pop()# pop the currently min node
-            k-=1
-            if not k:#means the current roor is the kth smallest element
-                return root.val
-            root=root.right# choose the node that are bigger than left and mid
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        #T=O(m*n) S=O(n) n=len of equations, m=len of queries
+        graph=defaultdict(defaultdict)#the graph represented as an adjacency list   graph[a][b] = value means a / b = value
+
+        def backtrack_evaluate(currentNode:str, targetNode:str, product:float, visited:set)->int:
+            visited.add(currentNode)
+            neighbors=graph[currentNode]
+            res=-1# We assume the ans does not exist
+
+            if targetNode in neighbors:
+                res=product*neighbors[targetNode]
+            else:
+                #explore all neighbor recursively
+                for neighbor in neighbors:
+                    if neighbor in visited:
+                        continue
+                    res=backtrack_evaluate(neighbor,targetNode,product*neighbors[neighbor],visited)
+                    if res!=-1:
+                        break# we find the path
+            visited.remove(currentNode)
+            return res
+        #Step1: build the graph from given equations and values
+        for equation,value in zip(equations,values):
+            graph[equation[0]][equation[1]]=value
+            graph[equation[1]][equation[0]]=1/value
+
+        #Step2: answer each query using DFS search
+        result=[]
+        for (dividend,divisor) in queries:
+            if dividend not in graph or divisor not in graph:
+                result.append(-1)
+            elif dividend==divisor:
+                result.append(1)
+            else:
+                visited=set()
+                result.append(backtrack_evaluate(dividend,divisor,1,visited))
+
+        return result
+
 
 
 
@@ -29,12 +49,4 @@ class Solution:
 
 if __name__ == '__main__':
     solution=Solution()
-    node1=TreeNode(3)
-    node2=TreeNode(1)
-    node3=TreeNode(4)
-    node4=TreeNode(2)
-
-    node1.left=node2
-    node1.right=node3
-    node2.right=node4
-    print(solution.kthSmallest(node1,1))
+    print(solution.calcEquation([["a","b"],["b","c"]],[2.0,3.0],[["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]))
