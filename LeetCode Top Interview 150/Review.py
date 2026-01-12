262e1c5499fb6937a5809f016f06830387c257a1
niuji <niujiazhen26@163.com>
2026-01-11 17:07:26 -0800

    2026/1/11 Review BackTracking New:52

--- LeetCode Top Interview 150/Review.py
+++ LeetCode Top Interview 150/Review.py
@@ -1,29 +1,37 @@
-from collections import deque
 from typing import List
 
 
 class Solution:
-    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
-        # Initialize a BFS queue with the starting gene and 0 mutations so far
-        queue=deque()
-        queue.append((start,0))
-        visited=set()
-        visited.add(start)
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows=len(board)
+        cols=len(board[0])
+        visited=[[False]*cols for _ in range(rows)]
 
-        while queue:
-            # pop the current str
-            node,steps=queue.popleft()
-            if node==end:
-                return steps
-            for char in "ACGT":
-                for i in range(len(node)):
-                    newNode=node[:i]+char+node[i+1:]
-                    if newNode not in visited and newNode in bank:
-                        queue.append((newNode,steps+1))
-                        visited.add(newNode)
+        def backTracking(path:List[str], x:int, y:int)->bool:
+            # End Status
+            if len("".join(path))==len(word):
+                return True
+            # False Case
+            if x<0 or x>=rows or y<0 or y>=cols:
+                return False# Out of boundary
+            if visited[x][y]:
+                return False# Already in path
+            if board[x][y]!=word[len(path)]:
+                return False# the letter is not what we need in current position, just skip it to reduce time complexity
 
-        return -1
+            visited[x][y]=True
+            path.append(board[x][y])
+            res=backTracking(path,x+1,y) or backTracking(path,x-1,y) or backTracking(path,x,y+1) or backTracking(path,x,y-1)
+            visited[x][y]=False
+            path.pop()
 
+            return res
+
+        for i in range(rows):
+            for j in range(cols):
+                if backTracking([],i,j):
+                    return True
+        return False
 if __name__ == '__main__':
     solution=Solution()
-    print(solution.minMutation("AACCTTGG", "AATTCCGG", ["AATTCCGG","AACCTGGG","AACCCCGG","AACCTACC"]))
+    print(solution.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED"))
