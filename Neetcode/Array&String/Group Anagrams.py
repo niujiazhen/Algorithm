5f321ea188f17444773274ebfa02f98af3917846
niuji <niujiazhen26@163.com>
2026-02-05 00:40:53 -0800

    2026/02/05 Neetcode Blind75

--- Neetcode/Array&String/Group Anagrams.py
+++ Neetcode/Array&String/Group Anagrams.py
@@ -1,11 +1,25 @@
+from email.policy import default
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Edge Case
+        if not strs:
+            return [[]]
 
+        # T=O(m*n) n is the length of strs and m is length of longest str
+        # S=O(n)
+        res=defaultdict(list)# map letter frequency ——> word
 
+        for s in strs:
+            freq=[0]*26# O(1)
+            for i in range(len(s)):
+                freq[ord(s[i])-ord("a")]+=1
+            res[tuple(freq)].append(s) # we change list to tuple because list cannot be a key in python
 
+        return list(res.values())
 if __name__ == '__main__':
     solution=Solution()
     print(solution.groupAnagrams(["act","pots","tops","cat","stop","hat"]))
