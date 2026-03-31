156a878f8f1e924d749b741413dceabef3eff40b
niujiazhen <niujiazhen26@163.com>
2026-03-30 19:43:31 -0700

    LeetCode Hot100 2,19,21

---
+++ LeetCodeHot100/链表/19. Remove Nth Node From End of List.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head: ListNode, n: int)->ListNode:
+    # 快慢指针
+    fast=slow=head
+
+
+    for i in range(n-1):
+        fast=fast.next
+
+    while fast:
+        fast=fast.next
+        slow=slow.next
+
+    # 现在slow节点是需要删除的
+    slow.next=slow.next.next
+
+    return head
+
+
+
+
+
+
+if __name__ == '__main__':
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    print(removeNthFromEnd(node1, 2))
