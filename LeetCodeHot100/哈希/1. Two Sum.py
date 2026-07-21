7e5ec33bc81bf5523e0dc5900affc71fcd648526
niujiazhen <niujiazhen26@163.com>
2026-07-22 00:56:58 +0800

    LeetCode Hot100：1

--- LeetCodeHot100/哈希/1. Two Sum.py
+++ LeetCodeHot100/哈希/1. Two Sum.py
@@ -1,19 +1,11 @@
 from typing import List
 
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # No Edge Case
-
-        # Basic Solution
-        # T=O(n) S=O(n)
-        hash={}# value->index
-        for i in range(len(nums)):
-            if target-nums[i] in hash:
-                return [hash[target-nums[i]],i]
-            hash[nums[i]]=i
-
+def twoSum(nums: List[int], target: int)->List[int]:
+    hash={}# 存储value->index
+    for i in range(len(nums)):
+        if target-nums[i] in hash:# 如果当前nums[i]需要的数字在hash中，直接返回答案
+            return [hash[target-nums[i]],i]
+        hash[nums[i]]=i# 加入哈希值
 
 if __name__ == '__main__':
-    solution=Solution()
-    print(solution.twoSum([2,7,11,15],9))
+    print(twoSum([2, 7, 11, 15], 9))
